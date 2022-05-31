5c11019cb7790a610eabde59d4e646411ae8c7ad
blamet <arturbaldoramos@gmail.com>
2022-05-31 14:36:19 -0300

    password recovery

---
+++ src/app/password-recovery/password-recovery-tab.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpService } from '../services/http.service';
+
+@Component({
+  selector: 'app-password-recovery-tab',
+  templateUrl: './password-recovery-tab.component.html',
+  styleUrls: ['./password-recovery-tab.component.css']
+})
+export class PasswordRecoveryTabComponent implements OnInit {
+
+  token: string;
+
+  constructor(
+    private _snackBar: MatSnackBar,
+    private httpService: HttpService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+    })
+  }
+
+  ngOnInit(): void {
+    if (this.httpService.isAuthenticated()) {
+      this.router.navigate([""])
+    }
+
+  }
+
+  backLogin() {
+    this.httpService.logout();
+    this.router.navigate(["login"]);
+  }
+
+  async changePassword(newPassword: string, newPasswordConfirm: string,) {
+    if (newPassword === newPasswordConfirm) {
+      const res = await this.httpService.post('change-password', {
+        token: this.token,
+        password: newPassword
+      });
+    }
+  }
+
+}
